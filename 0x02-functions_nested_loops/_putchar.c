3720a294abcf475967896949e74325a7a80a57b7
/PrettyOkuhle </empressnoziphokuhle@gmail.com>
2023-05-06 06:49:53 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 /**
  * _putchar - writes the character c to stdout
- * CC| The character to print
+ * @c: The character to print
  *
  * Return; On success 1.
  * On error, -1 returned, and is set appropriately.
